b69c3adc3b63c16189abdc28175153e745f1e477
Shivadubal26 <72974708+Shivadubal26@users.noreply.github.com>
2022-10-09 12:34:30 +0530

    prime.py

--- Python/prime.py
+++ Python/prime.py
@@ -1,8 +1,6 @@
-# Program to check if a number is prime or not
-
 
 # To take input from the user
-num = int(input("Enter a number: "))
+#num = int(input("Enter a number: "))
 
 # define a flag variable
 flag = False
